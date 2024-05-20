93ed36e4e6a3c31b9c0f917c06034ff7138e4f65
monsieurjonees <phil.reitzjones@me.com>
2024-05-20 11:40:39 -0500

    Shadows et al

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ export function changeTheme(theme: string) {
     if (themeElement != null) {
         document.head.removeChild(themeElement)
     }
-    if (theme == "light") { return; }
-
-    var newTheme = document.createElement("link")
-    newTheme.rel = "stylesheet"
-    newTheme.href = `/src/themes/${theme}.css`
-    newTheme.id = "themesheet"
-    document.head.appendChild(newTheme)
+    if (theme != "light") { 
+        var newTheme = document.createElement("link")
+        newTheme.rel = "stylesheet"
+        newTheme.href = `/src/themes/${theme}.css`
+        newTheme.id = "themesheet"
+        document.head.appendChild(newTheme)
+    }
 }
